c718bd15bc532f0fde6f5eab2287fb88d3b908e3
jtywa <65796752+jtywa@users.noreply.github.com>
2023-04-08 01:13:41 -0500

    added auto-hide feature to searchbar

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,31 @@
-import { ChangeEvent, SetStateAction, useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './SearchBar.css'
+import { motion, AnimatePresence } from 'framer-motion'
 import { RxMagnifyingGlass } from 'react-icons/rx'
 
 export default function SearchBar() {
 
   const [query, setQuery] = useState('');
+  const [lastScroll, setLastScroll] = useState('up');
+
+
+
+  useEffect(() => {
+    let waiting = false;
+    let scrollPos = 0;
+    function handleScroll(){
+      if (!waiting) {
+        let newScrollPos = window.scrollY;
+        newScrollPos > scrollPos ? setLastScroll("down") : setLastScroll("up");
+        scrollPos = newScrollPos;
+        waiting = true;
+        setTimeout(() => {waiting = false}, 100);
+        console.log(lastScroll)
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+    return (() => window.removeEventListener('scroll', handleScroll));
+  },[])
 
   function search() {
     if (query === '') return;
@@ -12,11 +33,16 @@ export default function SearchBar() {
   }
 
   return (
-    <div className="search shadow">
-        <input onChange={e => setQuery(e.target.value)} type="text" />
-        <button className="shadow-small" onClick={search}>
-          <RxMagnifyingGlass/>
-        </button>
-    </div>
+
+    <AnimatePresence>
+      {lastScroll == "up" &&
+        <motion.div className="search shadow" key="nav-menu" initial={{top: "-8rem"}} animate={{ top: "0"}} exit={{top: "-8rem"}}>
+            <input onChange={e => setQuery(e.target.value)} type="text" />
+            <button className="shadow-small" onClick={search}>
+              <RxMagnifyingGlass/>
+            </button>
+        </motion.div>
+      }
+    </AnimatePresence>
   )
 }
