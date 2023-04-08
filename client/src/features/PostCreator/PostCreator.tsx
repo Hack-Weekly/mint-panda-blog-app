838488854ff66e81c1d47de448ab7e10ab8eb5c5
jtywa <65796752+jtywa@users.noreply.github.com>
2023-04-07 21:57:27 -0500

    added some functionality to PostCreator

--- client/src/features/PostCreator/PostCreator.tsx
+++ client/src/features/PostCreator/PostCreator.tsx
@@ -1,19 +1,43 @@
 import './PostCreator.css'
+import { useState } from 'react'
 
 export default function PostCreator() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [tags, setTags] = useState('');
+
+  function submitPost() {
+    //input validation goes here
+
+    let tagsArray = tags.split(',');
+
+    let postObject = {
+      title: title,
+      // image: image,
+      body: body,
+      tags: tagsArray,
+    }
+
+    // send to backend
+
+    setTitle('');
+    setBody('');
+    setTags('');
+  }
+
   return (
-    <div className="post-creator shadow">
+    <form className="post-creator shadow">
         <h2>New Post</h2>
-        <input type="text" name="title" id="title" placeholder="Title"/>
+        <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" name="title" id="title" placeholder="Title"/>
         <div className="add-image">
           <span>Add an image?</span> 
           <button className="shadow-small">Browse</button>
         </div>
-        <textarea rows={6} name="body" id="body" placeholder="Body"/>
-        <input type="text" name="tags" id="tags" placeholder="Tags, seperated by comma" />
+        <textarea onChange={(e) => setBody(e.target.value)} value={body} rows={6} name="body" id="body" placeholder="Body"/>
+        <input onChange={(e) => setTags(e.target.value)} value={tags} type="text" name="tags" id="tags" placeholder="Tags, seperated by comma"/>
         <div className="post-creator-footer">
-            <button className="shadow-small">Post</button>
+            <button type="button" onClick={submitPost} className="shadow-small">Post</button>
         </div>
-    </div>
+    </form>
   )
 }
