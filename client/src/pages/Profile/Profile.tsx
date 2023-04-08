c7f7c3a99a2b70eebc2c5695d539e8e8ef686383
jtywa <65796752+jtywa@users.noreply.github.com>
2023-04-08 02:09:16 -0500

    added profile styling and logic

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,17 +1,30 @@
 import './Profile.css'
+import { useState } from 'react';
+import { FiEdit2, FiCheckSquare } from 'react-icons/fi';
+
+const dummyData = {
+  username: "John Smith",
+  avatar: "",
+}
 
 export default function Profile() {
-  const dummyData = {
-    username: "John Smith",
-    avatar: "",
-  }
-  
+
+  const [isEditingUsername, setIsEditingUsername] = useState(false);
+  const [currentUsername, setCurrentUsername] = useState(dummyData.username);
+
   return (
     <div className="profile">
       <img className="profile-avatar" src={dummyData.avatar}></img>
       <button>Edit Avatar</button>
-      <div className="profile-username">{dummyData.username}</div>
-      <button>Edit Username</button>
+      <div style={{display: 'flex', alignItems: 'center', gap: '.5rem'}}>
+        {!isEditingUsername ? <span className="profile-username">{currentUsername}</span>
+        : <input onChange={e => setCurrentUsername(e.target.value)} value={currentUsername}></input>
+        }
+        <button className="edit-button" onClick={() => setIsEditingUsername(!isEditingUsername)}>
+          {isEditingUsername ? <FiCheckSquare/> : <FiEdit2/>}
+        </button>
+      </div>
+
       <button>Log Out</button>
     </div>
   )
